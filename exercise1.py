5ebf11c316d62caf8563afc029ee14a3131abb22
gideon clottey <gideonclottey@gmail.com>
2023-02-05 00:11:11 +0000

    Assignment exercise done

---
+++ exercise1.py
@@ -0,0 +1,20 @@
+"""
+Exercise 04:
+Write a script called exercise1.py that includes assignment statements to perform the following operations with the variables a=1.14, b, and c:
+
+Adds 2 to a and assigns the result to b
+Multiplies b by 4 and assigns the result to a
+Divides a by 3.14 and assigns the result to b
+Subtracts 8 from b and assigns the result to c
+Display the final values of a, b and c
+"""
+
+a=1.14
+b= a +2
+a = b*4
+b = a/3.14
+c =b-8
+
+print(f'The value of a is: {a}')
+print(f'The value of b is:{b}')
+print(f'The value of c is:{c}')
